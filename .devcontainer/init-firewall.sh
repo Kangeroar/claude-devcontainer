40c740a1a78110c1b04a2df24835d59733803621
kangeroar <jacksonw888@hotmail.com>
2026-03-28 18:53:38 +0000

    feat: add ollama to domain safelist

--- .devcontainer/init-firewall.sh
+++ .devcontainer/init-firewall.sh
@@ -72,7 +72,7 @@ for domain in \
     "statsig.com" \
     "marketplace.visualstudio.com" \
     "vscode.blob.core.windows.net" \
-    "update.code.visualstudio.com"; \
+    "update.code.visualstudio.com" \
     "ollama.com"; \ 
     do echo "Resolving $domain..."
     ips=$(dig +noall +answer A "$domain" | awk '$4 == "A" {print $5}')
